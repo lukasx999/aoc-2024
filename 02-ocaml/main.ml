fd696d3507b0747e2f63f60da8d7afacda3564e7
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2024-12-02 16:29:33 +0100

    .

--- 02-ocaml/main.ml
+++ 02-ocaml/main.ml
@@ -1,33 +1,37 @@
 open Printf
 
 
+let filename = "example.txt"
+(* let filename = "input.txt" *)
+
+
 let iter_line_by_line filename callback =
+
+    let safe_count = ref 0 in
+
     let file = open_in filename in
     try
         while true do
             let line = input_line file in
-            callback line;
+
+            if callback line then
+                safe_count := !safe_count + 1
+            else ()
+
         done
-    with End_of_file -> close_in file
+    with End_of_file -> close_in file;
+
+    !safe_count
 
 
-
-let rec append_to_list list string index =
-    if index == String.length string then
-        list
-    else
-        let new_list =
-            match (String.get string index) with
-            | ' ' -> list
-            | c -> int_of_string (String.make 1 c) :: list
-        in
-        append_to_list new_list string (index+1)
 
 
 
 (* "1 2 3" -> [ 1 ; 2 ; 3 ] *)
-let string_to_list input_string =
-    List.rev (append_to_list [] input_string 0)
+let rec string_to_list string =
+    let l = String.split_on_char ' ' string in
+    List.map (fun x -> int_of_string x) l
+
 
 
 
@@ -41,32 +45,68 @@ let is_increasing a b =
 
 
 
-let callback_iter_number number =
-    printf "%d " number
+let compare_items_part_two numbers callback =
+
+    let problem_dampened = ref false in
+    let is_safe = ref true in
+    let index = ref 0 in
+
+    while !index < (List.length numbers)-1 do
+        let item = (List.nth numbers !index) in
+        let item_next = (List.nth numbers (!index+1)) in
+
+
+        if not (callback item item_next) then
+            if not !problem_dampened then
+                problem_dampened := true
+            else
+                is_safe := false
+        else ();
+
+        index := !index + 1
+    done;
+
+    !is_safe
+
+
+
+
+let compare_items_part_one numbers callback =
+
+    let is_safe = ref true in
+    let index = ref 0 in
+
+    while !index < (List.length numbers)-1 do
+        let item = (List.nth numbers !index) in
+        let item_next = (List.nth numbers (!index+1)) in
+
+        if not (callback item item_next) then
+            is_safe := false
+        else ();
+
+        index := !index + 1
+    done;
+
+    !is_safe
+
+
+
+
 
 
 let callback_iter_line line =
     let numbers = string_to_list line in
 
-    let is_safe = ref false in
-    let index = ref 0 in
+    (* let inc = compare_items_part_one numbers is_increasing in *)
+    (* let dec = compare_items_part_one numbers is_decreasing in *)
+    let inc = compare_items_part_two numbers is_increasing in
+    let dec = compare_items_part_two numbers is_decreasing in
 
-
-    while index < List.length numbers do
-    let item = (List.nth numbers !index) in
-    let item_next = (List.nth numbers !index+1) in
-    done
-
-    is_increasing (item item_next)
-
-
-
-
-    (* ignore (List.iter callback_iter_number numbers); *)
-
-    printf "\n"
+    inc || dec
 
 
 
 let () =
-    ignore (iter_line_by_line "example.txt" callback_iter_line);
+    let safe_count =
+        iter_line_by_line filename callback_iter_line in
+    printf "safe: %d\n" safe_count;
